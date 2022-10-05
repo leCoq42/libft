c129c6fddd8d72f179e04c40f97e3f5c4c07fc8f
Martin Haan <mhaan@f1r6s7.codam.nl>
2022-10-05 14:42:39 +0200

    added alnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,28 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_isdigit.c                                       :+:    :+:            */
+/*   ft_isalnum.c                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mhaan <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/10/05 14:14:33 by mhaan         #+#    #+#                 */
-/*   Updated: 2022/10/05 14:40:07 by mhaan         ########   odam.nl         */
+/*   Created: 2022/10/05 14:24:32 by mhaan         #+#    #+#                 */
+/*   Updated: 2022/10/05 14:42:21 by mhaan         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isdigit(int c)
+int	ft_isalnum(int c)
 {
 	if (c >= '0' && c <= '9')
+		return (1);
+	else if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
 	return (0);
 }
 
-/* # include	<stdio.h>
+/*# include	<stdio.h>
 # include	<ctype.h>
 int	main(void)
 {
-	char c = '1';
+	char c = '!';
 
-	printf("ft: %i\n", ft_isdigit(c));
-	printf("test: %i", isdigit(c));
-} */
+	printf("ft: %i\n", ft_isalnum(c));
+	printf("test: %i", isalnum(c));
+}*/
