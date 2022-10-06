aa44c4d94a271b92865e7c142f1cefa24e927eea
Martin Haan <mhaan@f1r5s19.codam.nl>
2022-10-06 11:32:48 +0200

    added strlen

---
+++ ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strlen.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mhaan <marvin@codam.nl>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/06 11:19:12 by mhaan         #+#    #+#                 */
+/*   Updated: 2022/10/06 11:30:04 by mhaan         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned long	ft_strlen(const char *s)
+{
+	unsigned long	i;
+
+	i = 0;
+	while(s[i])
+		i++;
+	return (i);
+}
+
+/* # include	<stdio.h>
+# include	<string.h>
+int	main(void)
+{
+	char *c = "";
+
+	printf("ft: %lu\n", ft_strlen(c));
+	printf("test: %lu\n", strlen(c));
+} */
