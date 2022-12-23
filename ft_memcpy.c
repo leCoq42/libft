52c54413853b8bda454b03cffe2c0090e52d5063
Martin Haan <mhaan@f0r4s10.codam.nl>
2022-12-23 17:03:58 +0100

    improvements on strchr and memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mhaan <mhaan@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/07 16:54:29 by mhaan         #+#    #+#                 */
-/*   Updated: 2022/11/24 15:19:38 by mhaan         ########   odam.nl         */
+/*   Updated: 2022/12/22 15:57:53 by mhaan         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	const char		*stmp = (const char *)src;
-	char			*dtmp;
+	const char	*stmp = (const char *)src;
+	char		*dtmp;
 
 	if (!dst && !src)
 		return (0);
 	dtmp = (char *)dst;
 	while (n-- > 0)
 		dtmp[n] = stmp[n];
-	return (dst);
+	return (dtmp);
 }
--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: martin <martin@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/09 15:08:46 by martin        #+#    #+#                 */
-/*   Updated: 2022/11/24 15:20:05 by mhaan         ########   odam.nl         */
+/*   Updated: 2022/12/23 17:03:24 by mhaan         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	if (*s == (char)c)
-		return ((char *)s);
+	char	*nstr;
+
+	nstr = (char *)s;
+	while (*nstr && *nstr != (char)c)
+		nstr++;
+	if (*nstr == (char)c)
+		return (nstr);
 	return (0);
 }
